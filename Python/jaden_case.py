cc4c3ecc6f9390c3998fe42561e45bb09768ee06
joshpeng <joshpeng@users.noreply.github.com>
2016-10-08 15:49:07 -0700

    Jaden Casing Strings refactor

--- Python/jaden_case.py
+++ Python/jaden_case.py
@@ -1,10 +1,2 @@
 def toJadenCase(string):
-	jadenCase = ''
-	for word in string.split(' '):
-		jadenCase += upperFirstLetter(word) + ' '
-	return jadenCase[0:-1]
-
-
-def upperFirstLetter(word):
-	print word
-	return word[0].upper() + word[1:]
+	return ' '.join(word.capitalize() for word in string.split())
