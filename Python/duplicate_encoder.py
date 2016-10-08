c072d5f326e92f623c46a97038e160a84742287c
joshpeng <joshpeng@users.noreply.github.com>
2016-10-08 15:42:51 -0700

    Refactor Duplicate Encoder solution

--- Python/duplicate_encoder.py
+++ Python/duplicate_encoder.py
@@ -1,13 +1,12 @@
+from collections import Counter
+
+
 def duplicate_encode(word):
-	characters = list(word.lower())
-	counter = {}
-	output = ""
-	for character in characters:
-		if character in counter:
-			counter[character] += 1
-		else:
-			counter[character] = 1
-	for character in characters:
+	word = word.lower()
+	counter = Counter(word)
+	output = ''
+
+	for character in word:
 		if counter[character] > 1:
 			output += ')'
 		else:
