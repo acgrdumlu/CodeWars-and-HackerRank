0f2a221414afff494bd49b1fdd409a27acd05678
joshpeng <joshpeng@users.noreply.github.com>
2016-10-08 15:47:11 -0700

    Love you refactor

--- Python/love_you_little_lot_passionately.py
+++ Python/love_you_little_lot_passionately.py
@@ -2,5 +2,4 @@ petals = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at al
 
 
 def how_much_i_love_you(nb_petals):
-	nb_petals = nb_petals - int(nb_petals / len(petals) * len(petals))
-	return petals[nb_petals - 1]
+	return petals[nb_petals % len(petals) - 1]
