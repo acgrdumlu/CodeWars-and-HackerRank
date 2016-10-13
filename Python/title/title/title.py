6464db48656266bb21b8b9d2d08c8fcbe34614a5
joshpeng <joshpeng@users.noreply.github.com>
2016-10-13 00:03:08 -0700

    Solved 'Title Case'

--- Python/title/title/title.py
+++ Python/title/title/title.py
@@ -1,2 +1,16 @@
 def title_case(title, minor_words=''):
-	return ''
+	if title is None or title == '':
+		return ''
+
+	words = title.split()
+	title = words[0].capitalize()
+	words.pop(0)
+
+	minor_words = minor_words.lower().split()
+	for word in words:
+		if word.lower() not in minor_words:
+			title += ' ' + word.capitalize()
+		else:
+			title += ' ' + word.lower()
+
+	return title
