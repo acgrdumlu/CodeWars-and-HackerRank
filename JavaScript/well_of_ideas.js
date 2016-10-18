7c07c4540e5ea3d614558abc2b341aaedc5759a1
joshpeng <joshpeng@users.noreply.github.com>
2016-10-18 00:59:12 -0700

    Lint 'Well of Ideas' JavaScript solution

--- JavaScript/well_of_ideas.js
+++ JavaScript/well_of_ideas.js
@@ -2,10 +2,9 @@ function well(array) {
 	let count = 0;
 	for (let i = 0; i < array.length; i++) {
 		if (array[i] === 'good') {
-			count++
+			count++;
 		}
 	}
 
-	let output = count >= 1 ? 'Publish!' : 'Fail!';
 	return count > 2 ? 'I smell a series!' : count >= 1 ? 'Publish!' : 'Fail!';
 }
