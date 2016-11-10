66d9020023e1e003cdac7e605e596b4f5f12dca1
joshpeng <joshpeng@users.noreply.github.com>
2016-11-10 00:22:21 -0800

    Reconfigured nosetest directories for repo

--- Python/bear_steady_gene/test/steady_tests.py
+++ Python/bear_steady_gene/test/steady_tests.py
@@ -5,6 +5,6 @@ from gene.steady import stabilize
 
 
 def test_steady1():
-    """Tests against the example provided by HackerRank"""
+    """Bear Steady Gene: Tests against the example provided by HackerRank"""
     orig_gene = 'GAAATAAA'
     assert_equal(stabilize(8, orig_gene), 5)
